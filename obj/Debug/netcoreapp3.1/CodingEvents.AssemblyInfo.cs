8cd409aa1a8e07448cf38a1258a3c00156516d88
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-12-02 22:51:20 -0600

    load Nuget Packeges , connect to the server, added migration text

--- obj/Debug/netcoreapp3.1/CodingEvents.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/CodingEvents.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7d01d3e4-c9f6-4337-90ef-2b456c740192")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodingEvents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
