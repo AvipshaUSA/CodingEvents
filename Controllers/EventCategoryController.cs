e262adc7b23402f22da070b54048282d36177baa
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-12-03 22:28:04 -0600

    ch19 studio complete. I also add a delete option for my own practice

--- Controllers/EventCategoryController.cs
+++ Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
@@ -22,8 +23,63 @@ namespace CodingEvents.Controllers
         [Route("EventCategory/Index")]
         public IActionResult Index()
         {
+      
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel(); // creating an instance of the class AddEventCategoryViewModel.cs of viewModel
+            return View(addEventCategoryViewModel);
+
+        }
+
+
+    
+        [HttpPost("EventCategory/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newEventCategory);
+                context.SaveChanges();
+
+                return Redirect("Index");
+            }
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteCategory()
+
+        {
+
+            /*  ViewBag.events = EventData.GetAll();*/ // we want to display the user what we have in list
+            ViewBag.categories = context.Categories.ToList(); // gonna do same as GetAll() whisch was in Data/EventData.cs now deleted
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult DeleteCategory(int[] eventIds) // create method in the same name of above to post the delete.
+        {
+            foreach (var eventId in eventIds)
+            {
+                //EventData.Remove(eventId); //gonna do same as Removel() whisch was in Data/EventData.cs now deleted
+                EventCategory theEvent = context.Categories.Find(eventId);
+                context.Categories.Remove(theEvent);
+            }
+
+            context.SaveChanges(); //gonna save everything 
+            return Redirect("Index");
         }
     }
 }
---
+++ ViewModel/AddEventCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.ViewModel
+{
+    public class AddEventCategoryViewModel
+    {
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be 3 and 50 charecters.")]
+        public string Name { get; set; }
+    }
+}
